daec4ee19782dd480e87d867d51395083ba7d1fc
l3mos <gabriellemos1901@gmail.com>
2025-03-21 19:41:46 +0000

    .

--- count_depressive.py
+++ count_depressive.py
@@ -1,41 +1,15 @@
+import torch
+from transformers import BertTokenizer, BertForSequenceClassification
 import pandas as pd
-import os
+from sklearn.model_selection import train_test_split
+import emoji
 
-# Carregar o arquivo CSV
-df = pd.read_csv('/scratch/gabriel.lemos/Bluesky-Depression/dataset/dataset_final_limpo_emoji.csv')  # Substitua 'arquivo.csv' pelo nome do seu arquivo
-
-# Contar a frequência dos valores na coluna desejada
-coluna = 'depressive'  # Substitua pelo nome da coluna desejada
-contagem = df[coluna].value_counts()
-
-# Exibir o resultado
-print(contagem)
-
-""" # Função para unir dois datasets e remover duplicatas com base em uma coluna específica
-def unir_datasets(arquivo1, arquivo2, coluna_chave, diretorio_saida='novo_diretorio'):
-    df1 = pd.read_csv(arquivo1)
-    df2 = pd.read_csv(arquivo2)
-    
-        # Remover a coluna 'ID' se existir
-    if 'ID' in df2.columns:
-        df2 = df2.drop(columns=['id'])
-    
-    # Concatenar os dois DataFrames
-    df_combined = pd.concat([df1, df2], ignore_index=True)
-    
-    # Remover duplicatas com base na coluna chave
-    #df_combined = df_combined.drop_duplicates(subset=[coluna_chave])
-    
-    # Criar diretório se não existir
-    os.makedirs(diretorio_saida, exist_ok=True)
-    
-    # Salvar o novo dataset no diretório especificado
-    caminho_saida = os.path.join(diretorio_saida, '/scratch/gabriel.lemos/Bluesky-Depression/dataset/dataset_final_limpo.csv')
-    df_combined.to_csv(caminho_saida, index=False)
-    
-    return df_combined, caminho_saida
-
-# Exemplo de uso
-novo_df, caminho = unir_datasets('/scratch/gabriel.lemos/Bluesky-Depression/dataset_final_f_emoji.csv', '/scratch/gabriel.lemos/Bluesky-Depression/dataset/scrapersdataset_limpo.csv', 'text')
-print(f'Dataset salvo em: {caminho}')
- """
+def remove_emojis(text):
+    return emoji.replace_emoji(text, "")
+# Carregar dataset
+df = pd.read_csv("/scratch/gabriel.lemos/Bluesky-Depression/dataset2_limpo.csv")
+df["text"] = df["text"].astype(str).fillna("")
+df["text"] = df["text"].apply(remove_emojis)  # Remover emojis
+df = df.drop_duplicates(subset=["text"]).reset_index(drop=True)
+df = df[df['text'].str.strip() != '']
+df = df[df['depressive'] == 1]  # Manter apenas os depressivos

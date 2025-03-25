41bf405144091c97c369d5de751f8581d0b29b79
l3mos <gabriellemos1901@gmail.com>
2025-03-25 20:57:39 +0000

    adicao do modelo roBERTa

--- func_aux.py
+++ func_aux.py
@@ -1,7 +1,15 @@
 import pandas as pd
 import emoji
 
-# Carregar o dataset CSV
+import torch
+
+if torch.cuda.is_available():
+    print(f"GPU disponível: {torch.cuda.get_device_name(0)}")
+else:
+    print("Nenhuma GPU disponível, rodando na CPU.")
+
+
+""" # Carregar o dataset CSV
 df = pd.read_csv("/scratch/gabriel.lemos/Bluesky-Depression/dataset_final.csv")
 
 # Função para remover emojis
@@ -13,3 +21,4 @@ df['text'] = df['text'].apply(remove_emojis)
 
 # Salvar o CSV limpo
 df.to_csv("dataset_final_f_emoji2.csv", index=False)
+ """
